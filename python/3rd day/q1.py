5fbabbffb5d6d956b3896d197c6606e1d5831868
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2025-12-23 10:58:37 +0530

    commit

--- python/3rd day/q1.py
+++ python/3rd day/q1.py
@@ -1,13 +1,16 @@
 import math
-a= int(input("Enter a number: "))
-b= int(input("Enter another number: "))
-c= int(input("Enter one more number: "))
+a = int(input("Enter the value of a = "))
+b = int(input("Enter the value of b = "))
+m = int(input("Enter the value of m = "))
+d = math.gcd(a, m)
 def gcd(a, b):
     while(b):
         a, b = b, a % b
     return a
-d = gcd(a, b)
-if d ==1:
-    print(f"{a} and {b} ")
+d = gcd(a, m)
+if d == 1:
+    print(f"The equation {a}x ≡ {b} (mod {m}) has a unique solution.")
+elif b % d == 0:
+    print(f"The equation {a}x ≡ {b} (mod {m}) has {d} solutions.")
 else:
-    print(f"{a} and {b} ")
+    print(f"Solution of the equation {a}x ≡ {b} (mod {m}) does not exist.")
