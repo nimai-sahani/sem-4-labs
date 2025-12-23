108794bc89bfcebfcc66c543ee023d774275a946
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2025-12-23 10:58:47 +0530

    hello

--- python/3rd day/rough.py
+++ python/3rd day/rough.py
@@ -1,13 +1,19 @@
-import math
-a= int(input("Enter a number: "))
-b= int(input("Enter another number: "))
-c= int(input("Enter one more number: "))
-def gcd(a, b):
-    while(b):
-        a, b = b, a % b
+a = int(input("Enter the value of a = "))
+b = int(input("Enter the value of b = "))
+m = int(input("Enter the value of m = "))
+
+# find gcd
+def gcd(a,b):
+    a,b = abs(a),abs(b)
+    while b != 0:
+        a,b = b,a%b
     return a
-d = gcd(a, b)
-if d ==1:
-    print(f"{a} and {b} ")
+d = gcd(a,m)
+
+# check existance
+if(d==1):
+    print(f"The equation {a}x={b} (mod {m}) has unique solution.")
+elif(b%d==0):
+    print(f"The equation {a}x{b} (mod {m}) has {d} solution.")
 else:
-    print(f"{a} and {b} ")
+    print(f"solution of the equation {a}x={b} (mod {m}) does not exist.")
